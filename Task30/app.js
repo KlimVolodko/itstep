0975b80cac774fba33c1ea6927b79e6468a9de35
Klim <elvvood@mail.ru>
2022-11-08 21:13:49 +0300

    'Task30'

--- Task30/app.js
+++ Task30/app.js
@@ -9,10 +9,98 @@
 8. Дан массив из n элементов и число k < n.Удалить элемент с номером k и сдвинуть на  одну позицию влево все элементы, следующие за ним.
 9. Дан массив из n элементов.Проверить, упорядочен ли массив по возрастанию или убыванию.
 10. Дан массив из n элементов.Удалить все повторяющие элементы(удалить и сдвинуть следующие за ним).
-*/
 
+СОЗДАНИЕ МАССИВА РАНДОМ
+const min = 50, max = 100, n = 5;
+let a = [];
+for (let i = 0; i < n; i++) {
+    a[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(a);    
+*/
 console.log(`Задача 1`)
-let a = [5, 6, 7, 3, 6, 54, 3, 7, 2];
+const min = -50, max = 100, n = 5;
+let a = [];
+for (let i = 0; i < n; i++) {
+    a[i] = Math.floor(Math.random() * (max - min + 1)) + min;
+}
+console.log(a);
+
 let k = 5;
-for(let i=0; )
-console.log(a[a.length - k]);
+for (let i = a.length - k; i < a.length; i++){
+    console.log(a[i]);
+}
+
+console.log(`Задача 2`)
+k = 5;
+for (let i = 0; i < a.length; i++) {
+    if (a[i] % k == 0) {
+        console.log(a[i]);
+    }  
+} 
+
+console.log(`Задача 3`) // сумма макс и мин
+let nmax;
+let maxElement = a[0];
+for (let i = 1; i < a.length; i++) {
+    if (a[i] > maxElement) {
+        maxElement = a[i];
+        nmax = i; //для задания 4, добавлен для того чтобы потом найти номер элемента 
+    }
+}
+console.log("Максимальный элемент равен " + maxElement);
+let nmin;
+let minElement = a[0];
+for (let i = 1; i < a.length; i++) {
+    if (a[i] < minElement) {
+        minElement = a[i];
+        nmin = i; //для задания 4, добавлен для того чтобы потом найти номер элемента 
+    }
+}
+console.log("Минимальный элемент равен " + minElement);
+console.log("сумма", maxElement + minElement);
+console.log(nmin, nmax);
+
+console.log(`Задача 4`) // поменять местами мин макс
+console.log(a);
+a[nmin] = maxElement;
+a[nmax] = minElement;
+console.log(a);
+
+console.log(`Задача 5`) // поделить массивы с четными нечетными
+b = [];
+c = [];
+let j1 = 0, j2 = 0;
+console.log(a);
+for (let i = 0; i < a.length; i++) {
+    if (a[i] %2 == 0) {
+        b[j1] = a[i];
+        j1++;
+    } else {
+        c[j2] = a[i];
+        j2++;
+    }
+} console.log(b); 
+console.log(c);
+
+console.log(`Задача 6`) // 
+console.log(a);
+for (let i = 0; i < a.length; i++) {
+    if (a[i] % 2 == 0) {
+        a[i] /= 2;
+    } else {
+        a[i] += 1;
+    }
+} 
+console.log(a);
+
+console.log(`Задача 7`) //  работает но оказалось неправильно
+console.log(a);
+let x = 5;
+for (let i = 0; i < a.length; i++) {
+    if (a[i] == x) {
+        console.log("есть");
+    } else {
+        console.log("нету");
+    }
+}
